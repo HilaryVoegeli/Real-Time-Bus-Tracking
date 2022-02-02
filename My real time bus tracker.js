f7b97f348b77a671b0b87765e1b9f01a7e172055
Hilary Voegeli <94722702+HilaryVoegeli@users.noreply.github.com>
2022-02-02 14:17:58 -0600

    update comments

--- My real time bus tracker.js
+++ My real time bus tracker.js
@@ -18,6 +18,7 @@ async function run(){
 	const locations = await getBusLocations();
   coordinates.push([(locations[0].attributes.longitude), (locations[0].attributes.latitude)]);
   coordinates1.push([(locations[1].attributes.longitude), (locations[1].attributes.latitude)]);
+	//so I can see the data
   console.log(coordinates);
   console.log(coordinates1);
 	// timer
@@ -33,28 +34,31 @@ async function getBusLocations(){
 }
 run(); 
 
+//make marker 1
 var marker = new mapboxgl.Marker()
    .setLngLat([-71.092761, 42.357575])
    .addTo(map);
   
+//make marker 2
 var marker1 = new mapboxgl.Marker()
     .setLngLat([-71.092761, 42.357575])
     .addTo(map);
 
+//move the markers
 function move(){
     setTimeout(() =>{
-    if(counter >= coordinates.length) return;
+    if(counter > coordinates.length) return;
     marker.setLngLat(coordinates[counter]);
     counter++;
     move();
-  }, 15000);
+  }, 15100);
  }
 
  function move1(){
     setTimeout(() =>{
-    if(counter >= coordinates1.length) return;
+    if(counter > coordinates1.length) return;
     marker1.setLngLat(coordinates1[counter]);
     counter++;
     move1();
-  }, 15000);
+  }, 15100);
  }
